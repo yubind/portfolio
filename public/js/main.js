15b7fab18eccda6c7128bfc02eacfd76ddcd1714
Yubin Kim <yubind91@gmail.com>
2018-10-18 20:58:12 +0000

    add styling

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,31 @@
-alert('hahaha');
+$(document).ready(function() {
+   //scroll animation 
+  $('a[href^="#"]').on('click', function(e) {
+    var target = $(this.getAttribute('href'));
+    
+    if( target.length ) {
+        e.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+  });
+    
+   
+   //highlight nav
+   $(window).scroll(function() {
+    var position  = window.pageYOffset;
+
+    $('.section').each(function() {
+      var target = $(this).offset().top;
+      var id = $(this).attr('id');
+      var navLinks = $('.nav-link');
+      var navHeight = $('.nav').height();
+      
+      if(position >= target - navHeight) {
+        navLinks.removeClass('current');
+        $('.nav .nav-item a[href="#' + id + '"]').addClass('current');
+      }
+    });
+   });
+});
